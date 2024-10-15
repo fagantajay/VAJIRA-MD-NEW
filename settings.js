ad6bd07002af0a03dab702894bf8eaa34c09ee00
Earrape <86729940+Earrape@users.noreply.github.com>
2024-10-15 15:16:09 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     
-SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=CYVHhKpQ#RbtauIqJkaKGDHqcYsDZB3KVGTaWd5WQOkImWywnivc' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'VAJIRA-MD=nAdzhISQ#nMvUkYhQLVIKdAkNMVCBRktgwnZuYeBZ33vZDXKlQVY' : process.env.SESSION_ID,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? '16582148118' : process.env.OWNER_NUMBER,   
 ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
 ONLY_ME: process.env.ONLY_ME === undefined ? 'false' : process.env.ONLY_ME,
